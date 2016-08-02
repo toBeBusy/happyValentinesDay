b1c8c3616f0b5fe8c9054cc45995f358f7b58cee
toBeBusy <18652613080@sina.cn>
2016-08-02 11:16:58 +0800

    add
    add js

--- script/forLove.js
+++ script/forLove.js
@@ -1,5 +1,13 @@
-var background = new Image();
-background.src = "img/love.jpg";
-background.onload = function(){
-	
+var winHeight =	$(window).height();
+var winWidth = $(window).width();
+var size;
+
+var setSize = function(){
+	if(winHeight > winWidth){
+		size = winWidth * 0.8;
+	} else {
+		size = winHeight * 0.8;
+	}
+	$("#loveImage").height(size);
+	$("#loveImage").width(size);
 }
